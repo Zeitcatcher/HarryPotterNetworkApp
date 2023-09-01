3e1637f1c420612c96fb8a628fdcbc2e481ed5a5
Zeitcatcher <ars.oksenoyt@gmail.com>
2023-08-31 18:11:25 -0700

    Initial Commit

---
+++ HarryPotterNetworkApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HarryPotterNetworkApp
+//
+//  Created by Arseniy Oksenoyt on 8/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
